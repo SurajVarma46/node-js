4a6418123f7b5474103f08842494471934f7b115
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-05-03 11:14:22 +0000

    Generated image file from text

---
+++ genimage/image.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+
+app.use(bodyparser());
+var path = require('path');
+const port = 80;
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.post('/', function(req, res) {
+ x=req.body.myText;
+var textToImage = require('text-to-image');
+textToImage.generate('Sample Text').then(function (dataUri) {
+  
+	let base64Image = x.split(';base64,').pop();
+	var fs = require('fs');
+	fs.writeFile('image.png', base64Image, {encoding: 'base64'}, function(err) {
+    	console.log('File created');
+});
+});
+});
+app.listen(port);
+console.log('Listening to port 80');
+
