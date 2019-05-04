2cf5f01fa9a7c6bdae1506ad668c9073e3174efb
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-05-04 04:47:44 +0000

    Updated Generation of image

--- genimage/image.js
+++ genimage/image.js
@@ -9,22 +9,18 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.post('/', function(req, res) {
-var x=req.body.myText;
-var textToImage = require('text-to-image');
-textToImage.generate(x).then(function (dataUri) {
+	var x=req.body.myText;
+	var textToImage = require('text-to-image');
+	textToImage.generate(x).then(function (dataUri) {
   
-	let x = dataUri.split(';base64,').pop();
-	var fs = require('fs');
-	fs.writeFile('image.png', x, {encoding: 'base64'}, function(err) {
-	res.send("Image file created");
-//		const pngToJpeg = require('png-to-jpeg');
-
-//let buffer = fs.readFileSync("./image.png");
-//pngToJpeg({quality: 90})(buffer)
-//.then(output => fs.writeFileSync("./image.jpeg", output));
-});
-});
-});
+		let x = dataUri.split(';base64,').pop();
+		var fs = require('fs');
+		fs.writeFile('image.png', x, {encoding: 'base64'}, function(err) {
+			res.sendFile(path.join(__dirname+'/image.png'));
+			//	res.send("Image file created");
+			});
+		});
+	});
 app.listen(port);
 console.log('Listening to port 80');
 
