3daa73f7dd8ffb97bc3aecf4404a0a4a7e3d11f0
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-05-04 07:01:02 +0000

    Image Style

--- genimage/image.js
+++ genimage/image.js
@@ -12,14 +12,18 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
 	var x=req.body.myText;
 	var textToImage = require('text-to-image');
-	textToImage.generate(x).then(function (dataUri) {
- 		let x = dataUri.split(';base64,').pop();
-		var fs = require('fs');
-		fs.writeFile('Image.png', x, {encoding: 'base64'}, function(err) {
-			res.sendFile(path.join(__dirname+'/Image.png'));
+	textToImage.generate(x,{
+			maxWidth:1024,
+			margin:100,
+		}).then(function (dataUri) {
+		
+ 			let x = dataUri.split(';base64,').pop();
+			var fs = require('fs');
+			fs.writeFile('Image.png', x, {encoding: 'base64'}, function(err) {
+					res.sendFile(path.join(__dirname+'/Image.png'));
+				});
 			});
 		});
-	});
 
 app.listen(port);
 console.log('Listening to port 80');
