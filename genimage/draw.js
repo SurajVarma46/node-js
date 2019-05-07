ed9672a038589ea918bff45235d20b89527a7d58
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-05-07 08:16:46 +0000

    Added author on image

--- genimage/draw.js
+++ genimage/draw.js
@@ -2,19 +2,22 @@ var { createCanvas, loadImage } = require("canvas");
 const height = 768;
 const width = 1024;
 const linewidth = 800;
-const lineheight = 40;
+const lineheight = 30;
 
 async function draw(text) {
 	var {createCanvas, loadImage } = require('canvas');
 	var canvas = createCanvas(width, height);
 	var ctx = canvas.getContext("2d");
+//	var author = 'Author';
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0,width, height);
 	ctx.font = "25px Impact";
 //	var meas = ctx.measureText(text);
  	ctx.fillStyle = "black";
 	ctx.textAlign = 'center';
-	wrapText(ctx, text, width/2, 300, linewidth, lineheight);
+	wrapText(ctx, text, width/2, 280, linewidth, lineheight);
+//	ctx.fillStyle="rgba(0,0,0,0.5";
+//	ctx.fillText(author,512,575);
 	//	ctx.fillText(text,width/2,300);
 	const image = await loadImage('images/logo.png');
 	ctx.drawImage(image, 936, 0,64,64 );
@@ -24,7 +27,7 @@ async function draw(text) {
 function wrapText(context, text, x, y, maxWidth, lineHeight) {
         var words = text.split(' ');
         var line = '';
-
+	var author = '---'+'Author';
         for(var n = 0; n < words.length; n++) {
           var testLine = line + words[n] + ' ';
           var metrics = context.measureText(testLine);
@@ -39,6 +42,10 @@ function wrapText(context, text, x, y, maxWidth, lineHeight) {
           }
         }
         context.fillText(line, x, y);
+	context.font = "italic 18pt Arial";
+	context.fillStyle = "rgba(0,0,0,0.5)";
+	context.fillText(author,width/2,y+lineheight+10);
       }
+	
 
 module.exports = draw;
