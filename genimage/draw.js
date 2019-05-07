a60594036cc1ede7e0b392eb1c7c0ca2b2c059cb
Suraj Varma Kakarlapudi <sukakarlapudi@gmail.com>
2019-05-07 09:32:24 +0000

    Added tags

--- genimage/draw.js
+++ genimage/draw.js
@@ -8,17 +8,12 @@ async function draw(text) {
 	var {createCanvas, loadImage } = require('canvas');
 	var canvas = createCanvas(width, height);
 	var ctx = canvas.getContext("2d");
-//	var author = 'Author';
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0,width, height);
 	ctx.font = "25px Impact";
-//	var meas = ctx.measureText(text);
  	ctx.fillStyle = "black";
 	ctx.textAlign = 'center';
 	wrapText(ctx, text, width/2, 280, linewidth, lineheight);
-//	ctx.fillStyle="rgba(0,0,0,0.5";
-//	ctx.fillText(author,512,575);
-	//	ctx.fillText(text,width/2,300);
 	const image = await loadImage('images/logo.png');
 	ctx.drawImage(image, 936, 0,64,64 );
 	return canvas;
@@ -27,25 +22,42 @@ async function draw(text) {
 function wrapText(context, text, x, y, maxWidth, lineHeight) {
         var words = text.split(' ');
         var line = '';
-	var author = '---'+'Author';
+	var author = '--- '+'Author';
+	var tags = 4;
         for(var n = 0; n < words.length; n++) {
-          var testLine = line + words[n] + ' ';
-          var metrics = context.measureText(testLine);
-          var testWidth = metrics.width;
-          if (testWidth > maxWidth && n > 0) {
-            context.fillText(line, x, y);
-            line = words[n] + ' ';
-            y += lineHeight;
-          }
-          else {
-            line = testLine;
-          }
+        	var testLine = line + words[n] + ' ';
+        	var metrics = context.measureText(testLine);
+        	var testWidth = metrics.width;
+        	if (testWidth > maxWidth && n > 0) {
+        		context.fillText(line, x, y);
+            		line = words[n] + ' ';
+            		y += lineHeight;
+          	}
+         	else {
+            		line = testLine;
+          	}
         }
         context.fillText(line, x, y);
 	context.font = "italic 18pt Arial";
 	context.fillStyle = "rgba(0,0,0,0.5)";
 	context.fillText(author,width/2,y+lineheight+10);
-      }
 	
+	context.font = "18pt Arial";
+	context.strokeStyle = "#000";
+	context.lineWidth = 0.1;
+	for (var i=0; i<tags; i++) {
+   		context.beginPath();
+   		context.fillStyle = "#e0e0e0";
+   		context.rect(128 + (i * 100) + ((8-tags)*50) ,y + lineheight + 50 , 80, 30);
+   		context.fill();
+   		
+		context.stroke();
+	
+   		context.fillStyle = "rgba(0,0,0,0.6)";
+		context.font = "italic 14pt Arial";
+   		context.fillText("tag", 168 + (i*100) + ((8-tags) * 50), y + lineheight + 70);
+	}
+}
+
 
 module.exports = draw;
